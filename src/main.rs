3a906f349aab8f170a50f2e95cf5eb16abb0bc42
Ttst <thibautstachnick@gmail.com>
2024-04-13 19:41:21 +0200

    add a method that use move on new task

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::thread;
 use std::time::Duration;
 
 fn two_task_at_once(){
-    thread::spawn(|| {
+    let manipulator = thread::spawn(|| {
         for i in 1..10{
             println!("hello n°{} from new task",i);
             thread::sleep(Duration::from_millis(1));
         }
     });
+    manipulator.join().unwrap();
     for i in 1..5 {
         println!("Hello n°{} from current task",i);
         thread::sleep(Duration::from_millis(1));
@@ -15,7 +16,19 @@ fn two_task_at_once(){
 
 }
 
+fn using_move_on_new_task(){
+    let v = vec![13,14,182];
+
+    let manipulator = thread::spawn(move|| {
+       println!("that's the vector {:?}",v)
+    });
+
+    drop(v);
+
+    manipulator.join().unwrap()
+}
+
 
 fn main() {
-    two_task_at_once()
+    using_move_on_new_task()
 }
